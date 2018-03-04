00165682a5f61846e22cbb9b35afe1802fb3f047
hmccullo <hunter.mccullough@wsu.edu>
2018-03-04 07:07:28 -0800

    Alpha R1

--- ReaLife/RealLife/app/src/main/java/com/realifetraining/reallife/MainActivity.java
+++ ReaLife/RealLife/app/src/main/java/com/realifetraining/reallife/MainActivity.java
@@ -79,19 +79,33 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        android.widget.TextView title =findViewById(R.id.text_title);
+        android.widget.TextView body =findViewById(R.id.text_body);
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+            title.setText("Welcome!");
+            body.setText("Did you spend all of high school complaining about how you never learned anything important to adult life? Well so did we! That's why we invented ReaLife, a program that allows you to learn the basics of adulthood. How to balance a checkbook, whatever that means, or become a wolf of wallstreet. Continue through our information blurbs, quizzes and even game to learn these, for once, useful skills!");
+        } else if (id == R.id.nav_balancing) {
+            title.setText("Balancing");
+            body.setText("Balancing your checkbook, surprisingly relates to budgeting almost directly. It is a way to keep track of what you have spent and how much you have left in your ‘checkbook’, bank account. You write down each bank transaction, ATM visit or check written; and subtract or add from your original total bank statement. This helps you have a good, or even perfect, estimate of what money you have to spend in your checking. More information can be found at: https://www.capitalone.com/financial-education/money-basics/balancing-budget/balance-your-checkbook/");
+        } else if (id == R.id.nav_budgeting) {
+            title.setText("Budgeting");
+            body.setText("Budgeting ensure that you will have enough money to pay for the essentials in life as well as essential non-essentials, like Redbull. Budgeting allows a person to keep track of what money they have coming in, at hand, and leaving; at all times. It allows a person to be more financially responsible and give over stability in their bank account. There are many apps and books that help a person learn what budgeting techniques they are most compatible with. More information at: https://www.daveramsey.com/blog/the-truth-about-budgeting");
+        } else if (id == R.id.nav_flowsheet) {
+            title.setText("Flow Sheets");
+            body.setText("A Flowsheet is a document that allows you to see the flow of your money. This document can be made with excel or any other table making program. It can be made to specify where each portion of your income flows. Making your bills, payments and excess money completely transparent to yourself for months to come. For more information: https://quickbooks.intuit.com/r/financial-management/free-cash-flow-statement-template-example-and-guide/");
+        } else if (id == R.id.nav_paying_bills) {
+            title.setText("Paying Bills");
+            body.setText("When you are renting or paying off your home, buying electricity, water, or heat; you have bills to pay. When paying bill, it is important to be proactive. First you need to check your mail often and make sure you keep track of all your bills and collect them in a safe spot. If you can not pay all your bills at once you can ask your banker for easy due dates to meet and finish paying them off. For more information read: https://www.wikihow.com/Pay-Your-Bills");
+        } else if (id == R.id.nav_stocks) {
+            title.setText("Stocks");
+            body.setText("A Stock Market displays the buyers and sellers of a company or cooperation. It is important to research past and predictions of a particular stock before investing. This allows you to have the ability to, hopefully, make money (Just avoid Bitcoin for the time being). Investing in Stock is not a required part of being an adult and does require risk. You can either win big, lose big, win then lose and vise versa over and over again or any combination. It a way to ‘gamble’ in a more professional manner.");
+        } else if (id == R.id.nav_subscriptions) {
+            title.setText("Subscriptions");
+            body.setText("Payments are required when a person has a subscription to a service or item. This could range from a subscription from the, almost holy, Chegg or that important educational subscription you have to the IEEE Spectrum. No matter what your service is, you must make these payments monthly, bimonthly or whatever your receipt says. Other payments include heating/cooling, electricity, and more. For more or any information look here: https://www.investopedia.com/terms/p/payment.asp");
+        } else if (id == R.id.nav_taxes) {
+            title.setText("Taxes");
+            body.setText("In the United States, citizens are required to pay taxes, unless they plan on living the next 3 years eating prison food, in prison. These taxes go towards paying government officials, building needed roads and even supplies for the U.S. military. Some states have sales tax which is a tax percentage on items sold. There are also income taxes on your yearly income and property taxes on the house and property. Once you are independent and finally weened off your parents and guardians you must pay these taxes. To do this you use a W-2 form. Don’t worry, if this is an acronym, it’s for something unimportant. You must use a W-2 to show proof of employment. For more information read: https://www.nerdwallet.com/blog/loans/student-loans/expert-advice-students-filing-taxes/");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
