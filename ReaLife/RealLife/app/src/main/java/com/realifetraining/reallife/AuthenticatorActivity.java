d09ab8d369f49fa33f87278898daa889e643af20
hmccullo <hunter.mccullough@wsu.edu>
2018-03-04 03:16:45 -0800

    Began the Android Studio Project Added Sign-In Functionality

---
+++ ReaLife/RealLife/app/src/main/java/com/realifetraining/reallife/AuthenticatorActivity.java
@@ -0,0 +1,29 @@
+package com.realifetraining.reallife;
+
+/**
+ * Created by hmccullo on 3/4/18.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+
+public class AuthenticatorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+                signin.login(AuthenticatorActivity.this, MainActivity.class).execute();
+            }
+        }).execute();
+    }
+}
